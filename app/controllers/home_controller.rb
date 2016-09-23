0e29deac8428650f243d54ea48bea1144ff12d0b
John Elder <johne4196@gmail.com>
2016-09-23 23:48:17 +0000

    added Tweet table and associations

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,18 @@
 class HomeController < ApplicationController
   def index
   end
+  
+  def users
+    @users = User.order(:user_name)
+    @tweets = Tweet.all
+  end
+  
+  def user_tweets
+    @item = params['item']
+    @tweets = Tweet.all
+    @users = User.all
+    #@fart = @users.find(params[:item])
+  end
+  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+  resources :tweets
   devise_for :users
   get 'home/index'
   root 'home#index'
+  get 'home/users'
+  #get :user_name => 'home#user_tweets'
+  get '/(:item)', to: "home#user_tweets", as: 'user_tweets'
+  
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
