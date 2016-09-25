f7ec0c15125987f7313aefc047a87678dbe51442
John Elder <johne4196@gmail.com>
2016-09-25 12:39:23 +0000

    tweaked validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :tweet
-  
+  validates :user_name,  presence: true
 end
